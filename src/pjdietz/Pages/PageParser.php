8cc55cacbeb2e49a702e7b3b12d3e80486fde435
PJ Dietz <pjdietz@gmail.com>
2014-12-30 11:28:47 -0500

    Extract content into a hash map.

--- src/pjdietz/Pages/PageParser.php
+++ src/pjdietz/Pages/PageParser.php
@@ -4,13 +4,16 @@ namespace pjdietz\Pages;
 
 class PageParser
 {
-    public function parse($str)
+    public function parse($subject)
     {
-        $metadata = $this->extractMetadata($str);
+        $metadata = $this->extractMetadata($subject);
         $metadata = $this->decodeMetadata($metadata);
+
+        $content = $this->extractContent($subject);
+
         return (object) [
             "metadata" => $metadata,
-            "content" => []
+            "content" => $content
         ];
     }
 
@@ -70,5 +73,54 @@ REGEX;
     protected function decodeMetadata($metadata)
     {
         return json_decode($metadata);
+    }
+
+    /**
+     * Retrieve the section representing the metadata.
+     *
+     * The default implementation extract all content between <!-- @metadata --> and <!-- @metadata end -->
+     *
+     * @param string $subject The original content to parse.
+     * @return string The extracted metadata.
+     */
+    protected function extractContent(&$subject)
+    {
+        // Build an array to fill with extracted content.
+        $content = [];
+
+        // Match content between <!-- section:{name} --> and <!-- section:{name} end -->
+        $regex = <<<'REGEX'
+{
+    # Opening marker. <!-- section:{name} -->
+    \<!--\s+section\:(?<name>[a-zA-Z0-9\-_]+)\s+--\>
+
+    # Rest of the line.
+    .*$\n
+
+    # \3: Content
+    (?<content>
+        (?>
+            (?!\<!--\s+section\:\1\s+end\s+--\> [ ]* \n) # Not a closing marker.
+            .*\n+
+        )+
+    )
+
+    # Closing marker. <!-- metadata end -->
+    \<!--\s+section\:\1\s+end\s+--\>
+
+}xm
+REGEX;
+
+        // Store the content to $content[name]
+        // Return "" to replace the entirty of the matched metadata block.
+        $callback = function ($matches) use (&$content) {
+            $content[$matches["name"]] = $matches["content"];
+            return "";
+        };
+
+        // Modify the passed $subject.
+        $subject = preg_replace_callback($regex, $callback, $subject);
+
+        return $content;
     }
 }
--- test/tests/PageParserTest.php
+++ test/tests/PageParserTest.php
@@ -27,6 +27,35 @@ STR;
         $page = $parser->parse($str);
         $this->assertEquals(json_decode($metadata), $page->metadata);
     }
+
+    public function testExtractsContent()
+    {
+        $str = <<<STR
+<!-- metadata -->
+{
+    "title": "Page Title"
+}
+<!-- metadata end -->
+
+<!-- section:main -->
+Main
+<!-- section:main end -->
+
+<!-- section:side -->
+Side
+<!-- section:side end -->
+
+STR;
+
+        $content = [
+            "main" => "Main\n",
+            "side" => "Side\n"
+        ];
+
+        $parser = new PageParser();
+        $page = $parser->parse($str);
+        $this->assertEquals($content, $page->content);
+    }
 }
 
 
